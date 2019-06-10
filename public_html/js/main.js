06855b3c89b0b103c6a5a0c742861a0f6ff04321
Fenris-c <anatolijv236@gmail.com>
2019-06-10 19:32:09 +0300

    Add adaptive, set hamburger

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,17 @@
+$('#hamburger').on('click', function(event) {
+    $('#mobileMenu').toggle().animate({'right': '0'}, 700);
+    $('#mobileMenu').toggleClass('menuScrolled');
+    $(this).toggleClass('mobileMenu_activated');
+    $('body').toggleClass('bodyNoScrolled');
+});
+
+$('#mobileMenu a.has_child').on('click', function(e) {
+    e.preventDefault();
+    $(this).next( ".topNav_2levelList_m").slideToggle( "slow", function() {
+        // Animation complete.
+    });
+});
+
 $(document).ready(function () {
 
     $("#slider__container").owlCarousel(
